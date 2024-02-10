f5f62fbcf7c0e3e86b62b3d6257e8d0115f23282
Saing <saing.sab@gmail.com>
2024-02-10 14:13:10 +0700

    add condition

---
+++ kotlin-foundation/src/Basics/Conditions.kt
@@ -0,0 +1,17 @@
+package Basics
+
+fun main() {
+//    if-else
+    var name = "Alex"
+    name = "Ayoung"
+    val result = if(name.length == 4) {
+        println("Name is fours charactors")
+        name
+    } else {
+        println("Name is not fours charactors")
+        name
+    }
+
+    println("result: $result")
+//    when
+}
---
+++ kotlin-foundation/src/Basics/Variables.kt
@@ -0,0 +1,27 @@
+package Basics
+fun main() {
+    val name: String = "Ayoung"
+    println(name)
+
+    var age: Int = 34
+    println(age)
+
+    age = 35
+    println(age)
+
+    val salary = 3000L
+    println(salary)
+
+//    String Imp
+    val course = "Kotlin Spring"
+    println("course: $course and the course length is ${course.length}")
+
+    val multiline = "ABC \n CDE"
+    println(multiline)
+
+    val multiline1 = """
+        ABC
+        CDE
+    """.trimIndent()
+    println(multiline1)
+}
