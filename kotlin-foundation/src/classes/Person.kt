ac3c9ed6bf502e79d035743849b42a63b90255f0
Saing <saing.sab@gmail.com>
2024-02-11 14:30:25 +0700

    add default contructor

--- kotlin-foundation/src/classes/Person.kt
+++ kotlin-foundation/src/classes/Person.kt
@@ -1,12 +1,20 @@
 package classes
 
-class Person {
+class Person(
+    val name: String = "", //Still possible of default value
+    val age: Int = 0
+) {
     fun action() {
         println("Person Walks")
     }
 }
 
 fun main() {
-    val person = Person()
+    val person = Person("Ayoung", 33)
     person.action()
+    println("Name: ${person.name} and the age is ${person.age}")
+
+//    init default contructor
+    val person1 = Person()
+    println("Name: ${person1.name} and the age is ${person1.age}")
 }
