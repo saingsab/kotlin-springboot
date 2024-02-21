c7711fa7bf7ac0c4d8cc58d965e140c5c3412750
Saing <saing.sab@gmail.com>
2024-02-21 11:07:23 +0700

    kotlin api greeting

---
+++ course-catalog-service/src/test/kotlin/com/kotlinspringboot/coursecatalogservice/CourseCatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinspringboot.coursecatalogservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseCatalogServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
